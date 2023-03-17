c9e31b1e0e3de318988106e2899dc2fd71423548
Tahmauri01 <tbobo@brooklynsteamcenter.org>
2023-03-17 10:42:19 -0400

    Finished website forreal

--- todo.py
+++ todo.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request, redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
 import pymysql.cursors
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "Tahmauri": generate_password_hash("hello"),
+    "Bobo": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
 
 todos=["Create a game", "Get a job"]
 
 @app.route("/")
+@auth.login_required
 def index():
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM `Todos` ORDER BY `Complete` ASC;')
